883185a544ee76048eef08608a19ae7fe08f7832
domuz86 <muamer.m86@gmail.com>
2022-04-05 20:21:58 +0200

    Repo09

---
+++ ex19/src/index.js
@@ -0,0 +1,22 @@
+//Only change code below this line
+
+var myNumbers ="";
+
+function myDoWhile() {
+
+    var i = 0;
+do {
+    myNumbers += i + ",";
+    i++;
+} while (i < 10);
+
+return myNumbers;
+
+}
+
+console.log(myDoWhile());
+
+//Only change code above this line
+
+
+module.exports = myDoWhile;
